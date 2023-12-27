a4457732d05d6394ee5d145d254825a5bb6406ff
satyendranath902 <satyendranath902@gmail.com>
2023-12-28 02:39:09 +0530

    eslint no-eval: 0

--- src/components/Deviceinfo.js
+++ src/components/Deviceinfo.js
@@ -3,13 +3,14 @@ import { useTheme } from './ThemeContext';
 
 function Deviceinfo() {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    console.log('Is Mobile Device:', isMobile);
     const { theme, toggleTheme } = useTheme();
 
     return <div className={`App ${theme}`}>
         <h4> User Agent: {navigator.userAgent}</h4>
         <h4> Screen Width: {window.screen.width}</h4>
         <h4> Screen Height: {window.screen.height}</h4>
-        <h4> Is Mobile Device:: {isMobile}</h4>
+        <h4> Is Mobile Device: {isMobile}</h4>
         <h4> Platform: {navigator.platform}</h4>
         <h4> Language: {navigator.language}</h4>
         <h4> Browser Version:: {navigator.appVersion}</h4>
